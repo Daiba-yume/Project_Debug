2efd81ba2f2d4de8933dfc5f6f01a577a85b4f3e
daiba <daiba_b@yahoo.com>
2024-08-29 19:47:54 +0200

    Add test error

--- Billed-app-FR-Front/src/__tests__/NewBill.js
+++ Billed-app-FR-Front/src/__tests__/NewBill.js
@@ -11,17 +11,16 @@ import { ROUTES } from "../constants/routes.js";
 
 describe("Given I am connected as an employee", () => {
   describe("When I am on NewBill Page", () => {
-    test("Then the bill should be created successfully", () => {
-      // Génère et insère le html(NewBill) dans le DOM
-      const html = NewBillUI();
-      document.body.innerHTML = html;
+    test("Then the bill should be created successfully", async () => {
+      // Génère et insère le HTML de la page NewBill dans le DOM
+      document.body.innerHTML = NewBillUI();
 
       // Simule la navigation en modifiant le contenu du DOM
       const onNavigate = (pathname) => {
         document.body.innerHTML = ROUTES({ pathname });
       };
 
-      // Config localStorage pour un utilisateur connecté
+      // Mock du localStorage pour simuler un utilisateur connecté
       Object.defineProperty(window, "localStorage", {
         value: localStorageMock,
       });
@@ -32,9 +31,10 @@ describe("Given I am connected as an employee", () => {
         document,
         onNavigate,
         store: mockStore,
-        localStorage: localStorageMock,
+        localStorage: window.localStorage,
       });
-      // Remplissage des champs du form
+
+      // Remplissage des champs du formulaire
       screen.getByTestId("expense-type").value = "Transports";
       screen.getByTestId("expense-name").value = "Vol Paris-Espagne";
       screen.getByTestId("datepicker").value = "2023-08-28";
@@ -43,13 +43,69 @@ describe("Given I am connected as an employee", () => {
       screen.getByTestId("pct").value = "20";
       screen.getByTestId("commentary").value = "Voyage d'affaires";
 
-      // Soumission du form
+      // Soumission du formulaire
       const form = screen.getByTestId("form-new-bill");
       form.addEventListener("submit", newBill.handleSubmit);
       fireEvent.submit(form);
 
-      // Vérifie que la facture a été créée après la soumission
+      // Vérifie que l'interface utilisateur a bien changé après la soumission
       expect(screen.getByText("Mes notes de frais")).toBeTruthy();
+    });
+
+    describe("When I submit and an error occurs", () => {
+      beforeEach(() => {
+        document.body.innerHTML = NewBillUI();
+      });
+
+      //Clean le DOM et réinitialise les mocks
+      afterEach(() => {
+        afterEach(() => {
+          document.body.innerHTML = "";
+          jest.clearAllMocks();
+        });
+
+        //Test la gestion des erreurs
+        const testErrorHandling = async (errorCode) => {
+          // Mock de la méthode update pour simuler une erreur
+          const updateMock = jest.fn().mockRejectedValue(new Error(errorCode));
+          // Remplace la méthode bills du store pour utiliser le mock
+          mockStore.bills = jest.fn(() => ({
+            create: jest.fn().mockResolvedValue({}),
+            update: updateMock,
+          }));
+
+          // New instance de NewBill avec le store mocké
+          const newBill = new NewBill({
+            document,
+            onNavigate: (pathname) => {
+              document.body.innerHTML = ROUTES({ pathname });
+            },
+            store: mockStore,
+            localStorage: window.localStorage,
+          });
+
+          // Select form depuis le DOM
+          const form = screen.getByTestId("form-new-bill");
+          form.addEventListener("submit", (e) => newBill.handleSubmit(e));
+
+          // Submit form and promise
+          fireEvent.submit(form);
+          await new Promise(process.nextTick);
+
+          // Vérifie que l'erreur simulée est bien levée
+          await expect(updateMock).toHaveBeenCalled();
+          // Vérifie que l'appel à update a échoué avec l'erreur attendue
+          await expect(updateMock).rejects.toThrow(new Error(errorCode));
+        };
+        // Teste la gestion des erreurs pour un code 404
+        test("Fetch fails with 404 error message", async () => {
+          await testErrorHandling("404");
+        });
+        // Teste la gestion des erreurs pour un code 404
+        test("Fetch fails with 500 error message", async () => {
+          await testErrorHandling("500");
+        });
+      });
     });
   });
 });
