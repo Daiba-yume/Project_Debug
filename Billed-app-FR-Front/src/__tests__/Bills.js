65b7b6eedf9526faf5f88fc6c86e3dcd96b12126
daiba <daiba_b@yahoo.com>
2024-08-30 10:12:59 +0200

    Correction et ajustements

--- Billed-app-FR-Front/src/__tests__/Bills.js
+++ Billed-app-FR-Front/src/__tests__/Bills.js
@@ -39,8 +39,7 @@ describe("Given I am connected as an employee", () => {
           /^(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$/i
         )
         .map((a) => a.innerHTML);
-      // trie par ordre antiChro (récent to ancien)
-      const antiChrono = (a, b) => new Date(b.date) - new Date(a.date);
+      const antiChrono = (a, b) => (a < b ? 1 : -1);
       const datesSorted = [...dates].sort(antiChrono);
       expect(dates).toEqual(datesSorted);
     });
